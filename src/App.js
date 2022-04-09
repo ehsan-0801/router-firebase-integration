74bc7472e8a853cd5b08671bc1e9892001157e2e
ehsan-0801 <ehsanulahmmed@gmail.com>
2022-04-09 20:11:27 +0600

    Sign in With Google using  pop up

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={ <Home></Home> }>Home</Route>
         <Route path="/home" element={ <Home></Home> }>Home</Route>
+        <Route path="/register" element={ <Register /> }>Home</Route>
         <Route path="/login" element={ <Login></Login> }>Login</Route>
       </Routes>
     </div>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useFirebase from '../../hooks/useFirebase';
 import './Header.css'
 const Header = () => {
+    const { user, handleSignOut } = useFirebase()
     return (
         <div className="Header">
             <nav>
-                <Link to="/" >Home</Link>
-                <Link to="/login" >Login</Link>
+                <Link to="/" >HOME</Link>
+                <Link to="/products" >PRODUCTS</Link>
+                <Link to="/orders" >ORDERS</Link>
+                <Link to="/register" >REGISTER</Link>
+                <span>
+                    { user?.displayName && user.displayName }</span>
+                {
+                    user?.uid
+                        ?
+                        <button onClick={ () => handleSignOut() }>sign out</button>
+                        :
+                        <Link to="/login">Login</Link>
+                }
             </nav>
-        </div>
+        </div >
     );
 };
 
